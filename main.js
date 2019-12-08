d2b433ec291ee765c7508653fb295cf6be5c478b
Tatsiana Odnovol <otanaotan@gmail.com>
2019-12-09 01:59:53 +0300

    Сверстала секцию слайдер и выпадающее меню

---
+++ main.js
@@ -0,0 +1,35 @@
+let button = document.querySelector("#button");
+let menu = document.querySelector("#overlay");
+let body = document.querySelector("body");
+function toggleMenu(){
+    button.classList.toggle('button__gamburger--active');
+    menu.classList.toggle('overlay--open');
+    body.classList.toggle('body-menu--active');
+}
+button.addEventListener("click", toggleMenu);
+
+
+
+
+
+// let menuOpenBurger = (function (buttonClass, menuClass){
+//     let button = document.querySelector("#button");
+//     let menu = document.querySelector("#overlay");
+//     let body = document.querySelector("body");
+//     let toggleMenu = function (e){
+//         button.classList.toggle('button__gamburger--active');
+//         menu.classList.toggle('overlay--open');
+//         body.classList.toggle('body-menu--active');
+//     }
+//     let addListeners = function (){
+//         button.addEventListener("click", toggleMenu);
+//     }
+//     return{
+//         openMenu: addListeners
+//     };    
+// })("#button", "#overlay" );
+// console.log(menuOpenBurger);
+// menuOpenBurger.openMenu();
+
+
+
