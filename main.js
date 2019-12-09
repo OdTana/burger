ce81349f8aa5dc0370e74df7651c3deddc73f81d
Tatsiana Odnovol <otanaotan@gmail.com>
2019-12-10 01:29:02 +0300

    Поправила ошибки валидации и путей, в аккордеоне попыталась реализовать JS

--- main.js
+++ main.js
@@ -15,14 +15,21 @@
 let menuOpenBurger = (function (buttonClass, menuClass){
     let button = document.querySelector(buttonClass);
     let menu = document.querySelector(menuClass);
-    // let body = document.querySelector("body");
+    let body = document.querySelector("body");
     let toggleMenu = function (e){
         button.classList.toggle('button__gamburger--active');
         menu.classList.toggle('overlay--open');
-        // body.classList.toggle('body-menu--active');
+        body.classList.toggle('body-menu--locked');
     }
     let addListeners = function (){
         button.addEventListener("click", toggleMenu);
+
+        menu.addEventListener("click", function(e){
+            target = e.target;
+            if(target.classList.contains("nav__link")){
+                toggleMenu(); 
+            }
+        })
     }
     return{
         openMenu: addListeners
@@ -33,3 +40,38 @@ menuOpenBurger.openMenu();
 
 
 
+
+let menuAccorddion = (function (options){
+    let menuTitle = document.querySelector(options.menuTitle);
+    let menuText = document.querySelector(options.menuText);
+
+    let accorddion = document.getElementById("menu__content");
+
+    let toggleAccorddion = function (e){
+        menuTitle.classList.toggle('menu__title--active');
+        menuText.classList.toggle('menu__text--active');
+        accorddion.classList.toggle('overlay--open');
+
+    }
+    let addAccorddion = function (){
+        menuTitle.addEventListener("click", toggleAccorddion);
+
+        accorddion.addEventListener("click", function(e){
+            target = e.target;
+            console.log(target);
+            if(target.classList.contains("menu__title")){
+                toggleAccorddion(); 
+            }
+        })
+    };        
+    return{
+        openAccorddion: addAccorddion
+    };    
+
+}) ({menuTitle:".menu__title",
+    menuText: ".menu__text"
+
+    });
+
+// console.log(menuAccorddion);
+menuAccorddion.openAccorddion();
